d9f39517c8e8acce1999861b345950ddac541c7a
Wujidadi <wujidadi@gmail.com>
2021-08-12 12:55:24 +0800

    修改 SCSS 檔案架構；HTML 樣式儘量以 Bootstrap CSS class 指定

--- views/Home/Main.view.php
+++ views/Home/Main.view.php
@@ -8,13 +8,29 @@
     <link rel="stylesheet" href="/libraries/Bootstrap/bootstrap.min.css">
     <script src="/libraries/Bootstrap/bootstrap.min.js"></script>
     <script src="/libraries/Vue/vue.global.prod.js"></script>
-    <link rel="stylesheet" href="/css/caterpillar.css">
+    <link rel="stylesheet" href="/css/caterpillar-main.css">
 </head>
 <body class="caterpillar caterpillar-default">
     <?php include viewPath('components.common.header'); ?>
     <?php include viewPath('components.common.left-side'); ?>
-    <main>
-        <div>Hello! Caterpillar.</div>
+    <main class="p-3" style="height: 2000px">
+        <div id="mainContent" class="tab-content">
+            <div id="home"        class="tab-pane fade show active">首頁</div>
+            <div id="circulation" class="tab-pane fade"            >借還書</div>
+            <div id="book"        class="tab-pane fade"            >圖書</div>
+            <div id="member"      class="tab-pane fade"            >會員</div>
+        </div>
     </main>
+    <script>
+        let circulationTab = document.querySelector('#circulationTab');
+        circulationTab.addEventListener('shown.bs.tab', function(event)
+        {
+            console.log('借還書分頁顯示');
+        });
+        circulationTab.addEventListener('hidden.bs.tab', function(event)
+        {
+            console.log('借還書分頁隱藏');
+        });
+    </script>
 </body>
 </html>
--- views/components/common/header.view.php
+++ views/components/common/header.view.php
@@ -1,5 +1,5 @@
 <header>
-    <div class="navbar fixed-top">
+    <div class="navbar fixed-top text-nowrap fs-r1.5 lh-sm px-3 py-3">
         <div><?php echo $headerTitle; ?></div>
     </div>
 </header>
